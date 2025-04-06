51fa97290e84ed404c19473d0ea741a5363b76c9
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-06 09:54:52 -0400

    Create ejercicio7.cpp

---
+++ ejercicio7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int x,t,z,y;
+    
+    cin>>x;
+    y=x-1;
+    z=x+1;
+    t=x*2;
+    cout<<"tu numero "<<x<<" esta entre "<<y<<" y "<<z<<". El doble de "<<x<<" es "<<t<<endl;   
+    
+   
+
+   
+
+    return 0;
+}
