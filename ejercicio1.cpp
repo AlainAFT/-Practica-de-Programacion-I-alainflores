394b34ef958d3010f0f9bb7b3774d72a33ce988d
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-05 21:58:45 -0400

    Update ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   printf("-------------------\n");
+   printf("|     /\"\"\"\"\"\\     |\n");
+   printf("|    /       \\    |\n");
+   printf("|    | 0   0 |    |\n");
+   printf("|    |   v   |    |\n");
+   printf("|    \\  ---  /    | \n");
+   printf("|     \\-----/     |\n");
+   printf("-------------------");
+   
+    return 0;
+}
+   
+  
+   
+    
+
 
