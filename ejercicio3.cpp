b53982732a3dda7be66265c95bc72870185d7478
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-05 22:10:48 -0400

    Create ejercicio3.cpp

---
+++ ejercicio3.cpp
@@ -0,0 +1,25 @@
+ #include <iostream>
+#include <string>
+
+int main() {
+    
+    char letter;
+    char task;
+
+   
+    letter = 'R';
+    task = 'A';
+    std::cout << "Se solicita al robot " << letter << " para " << task << "." << std::endl;
+
+    
+    letter = '9';
+    task = 'E';
+    std::cout << "Se solicita al robot " << letter << " para " << task << "." << std::endl;
+
+    return 0;
+}
+  
+  
+   
+    
+
