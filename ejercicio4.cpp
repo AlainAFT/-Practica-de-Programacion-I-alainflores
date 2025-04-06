3177f2d5e61308e481d2c40f4ec0474abbee6b53
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-04-05 22:16:37 -0400

    Create ejercicio4.cpp

---
+++ ejercicio4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+   string robot_name="RC-21\\\\M2"; 
+   cout <<"el robot "<<robot_name<<" tiene un mensaje. "<<endl;
+   robot_name="\"RC-21\\\\M2\"";
+   cout<<"el robot "<<robot_name<<" se esta incendiando."<<endl;
+    
+
+    return 0;
+}
+  
